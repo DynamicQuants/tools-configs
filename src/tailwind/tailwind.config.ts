39edd55939b674b021f5fc36992b5db307dd0e85
Carlos Cosming <carlos@kingly.cl>
2024-11-10 20:07:12 -0300

    ✨ feat: add tailwind config

---
+++ src/tailwind/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+// We want each package to be responsible for its own content.
+const config: Omit<Config, 'content'> = {
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
--- src/typescript/mod.ts
+++
@@ -1,5 +0,0 @@
-export { default as base } from './base.json' with { type: 'json' };
-export { default as nestjsLibrary } from './nestjs-library.json' with { type: 'json' };
-export { default as nestjs } from './nestjs.json' with { type: 'json' };
-export { default as nextjs } from './nextjs.json' with { type: 'json' };
-export { default as reactLibrary } from './react-library.json' with { type: 'json' };

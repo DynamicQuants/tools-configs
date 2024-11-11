90f7ed1c49132b3ba3cf03b199c03eef38472745
Carlos Cosming <carlos@kingly.cl>
2024-11-11 11:13:51 -0300

    🔧 chore: reconfig tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,13 @@ import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   treeshake: true,
-  splitting: true,
+  shims: true,
   entry: ['src/**/*.{css,js,json,ts}'],
-  outDir: 'dist',
   format: ['esm', 'cjs'],
   external: ['next', 'react', 'react-dom', 'jest', 'tailwindcss', 'tsup', '@types/jest'],
   dts: false,
   clean: true,
+  tsconfig: 'tsconfig.json',
+  sourcemap: true,
   ...options,
 }));

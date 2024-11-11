17730eb58695691fbbf57994ca10f3f97a0ecc43
Carlos Cosming <carlos@kingly.cl>
2024-11-11 11:25:47 -0300

    🔧 chore: change tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,11 +4,11 @@ export default defineConfig((options: Options) => ({
   treeshake: true,
   shims: true,
   entry: ['src/**/*.{css,js,json,ts}'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   external: ['next', 'react', 'react-dom', 'jest', 'tailwindcss', 'tsup', '@types/jest'],
   dts: false,
   clean: true,
   tsconfig: 'tsconfig.json',
-  sourcemap: true,
+  splitting: false,
   ...options,
 }));
